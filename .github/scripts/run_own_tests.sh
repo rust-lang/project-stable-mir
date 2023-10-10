8b4868efdcffde8b92591eca5271fc81cab00861
Celina G. Val <celinval@amazon.com>
2023-10-10 13:10:06 -0700

    Add custom toolchain section
    I also made it a bit easier to override our scripts toolchain

--- .github/scripts/run_own_tests.sh
+++ .github/scripts/run_own_tests.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 # Execute our own set of tests using a local `compiletest` tool based on `ui_test`.
+# This will run cargo under the repository path and, by default, will run the toolchain
+# specified in the `rust-toolchain.toml` file.
 set -e
 set -u
 
@@ -7,12 +9,14 @@ set -u
 TOOLS_BIN="${TOOLS_BIN:-"/tmp/smir/bin"}"
 # Assume we are inside SMIR repository
 SMIR_PATH=$(git rev-parse --show-toplevel)
+REPO_TOOLCHAIN=$(rustup show active-toolchain | (read toolchain _; echo $toolchain))
+TOOLCHAIN="${TOOLCHAIN:-${REPO_TOOLCHAIN}}"
 export RUST_BACKTRACE=1
 
 # Build stable_mir tools
 function build_smir_tools() {
-  pushd "${SMIR_PATH}"
-  cargo +nightly build -Z unstable-options --out-dir "${TOOLS_BIN}"
+  echo "#### Build tools. Toolchain: ${TOOLCHAIN}"
+  cargo +${TOOLCHAIN} build -Z unstable-options --out-dir "${TOOLS_BIN}"
   export PATH="${TOOLS_BIN}":"${PATH}"
 }
 
@@ -37,5 +41,6 @@ function run_tests() {
   done
 }
 
+pushd "${SMIR_PATH}" > /dev/null
 build_smir_tools
 run_tests
